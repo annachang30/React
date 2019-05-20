8e2313cab825134528c067f4bc788acdac44fd1e
Anna Chang <annachang30@gmail.com>
2019-05-19 18:06:04 -0700

    initial commit

---
+++ hello-dojo/index.js
@@ -0,0 +1,13 @@
+const helloDojo = React.createElement("div", null, React.createElement("h1", null, "Hello Dojo!"),
+    React.createElement("h2", null, "Things I need to do"),
+    React.createElement("li", null, "Learn React"),
+    React.createElement("li", null, "Climb Mt Everest"),
+    React.createElement("li", null, "Feed Bandit"));
+
+ReactDOM.render(helloDojo, document.getElementById("root"));
+
+
+
+
+
+
